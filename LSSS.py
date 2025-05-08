4417536813235871bb1bfffcf3751b9446528d5b
testenv603 <xxx>
2025-05-08 10:14:41 +0800

    feat(LSSS): 更新访问结构以支持更多参与者
    - 将参与者数量从3增加到11（10个数据用户和1个SGX）
    - 设计了新的访问结构，允许任意一个用户与SGX共同访问密钥

--- LSSS.py
+++ LSSS.py
@@ -19,8 +19,19 @@ if __name__ == "__main__":
 
     # multi secret sharing parameters
     secret_list = [key, 313, 501]
-    n_participants = 3
-    access_structures = [[[1, 3], [2, 3]], [[1, 2, 3]], [[1, 2, 3]]]
+    n_participants = 11  # 10 data user and one SGX
+    # 假设有10个用户和一个SGX，任意一个用户和SGX可以访问所有密钥
+    # 用户编号1-10，SGX编号11
+    # 访问结构：
+    # 用户 OR SGX
+    # (1 OR 2 OR 3 OR 4 OR 5 OR 6 OR 7 OR 8 OR 9 OR 10) AND (11)
+    access_structures = [[], [], []]
+    for i in range(n_participants - 1):
+        access_structures[0].append([i + 1, n_participants])
+
+    # 第2和第3个秘密的访问结构暂时任意
+    access_structures[1] = [[1, 3, 11], [5, 11]]
+    access_structures[2] = [[2, 4, 11], [6, 11], [8, 9, 10, 11]]
 
     # initialize Roy-Adhikari secret sharing algorithm
     dealer = multisecret.MultiSecretRoyAdhikari.Dealer(
