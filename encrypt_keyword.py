fea891b46c3b4cd44110581a502eec7727ea0baf
testenv603 <xxx>
2025-05-19 16:51:09 +0800

    feat(encrypt_keyword): 添加 AES 加密解密器的缓存
    - 为加密和解密操作引入了 lru_cache 装饰器
    - 创建了 _get_encrypt_cipher 和 _get_decrypt_cipher 函数来管理缓存
    - 这些更改可以提高性能，特别是在处理大量数据时

--- encrypt_keyword.py
+++ encrypt_keyword.py
@@ -1,10 +1,22 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from functools import lru_cache
+
+
+@lru_cache(maxsize=100, typed=True)
+def _get_encrypt_cipher(key: bytes):
+    return AES.new(key, AES.MODE_ECB)
+
+
+# 缓存解密 cipher 对象
+@lru_cache(maxsize=100, typed=True)
+def _get_decrypt_cipher(key: bytes):
+    return AES.new(key, AES.MODE_ECB)
 
 
 def symmetric_encryption_for_keyword(key, word):
     # 使用AES加密算法进行加密
-    cipher = AES.new(key, AES.MODE_ECB)
+    cipher = _get_encrypt_cipher(key)
     # 添加PKCS7填充
     padded_data = pad(word.encode(), AES.block_size)
     ciphertext = cipher.encrypt(padded_data)
@@ -13,7 +25,7 @@ def symmetric_encryption_for_keyword(key, word):
 
 def symmetric_decryption_for_keyword(key, word_enc):
     # 使用AES解密算法进行解密
-    cipher = AES.new(key, AES.MODE_ECB)
+    cipher = _get_decrypt_cipher(key)
     decrypted_padded = cipher.decrypt(word_enc)
     # 移除填充
     return unpad(decrypted_padded, AES.block_size).decode()
